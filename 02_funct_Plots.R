26271663a721dbcd7dec4cbe127819af9088436d
faithang025-cloud <faithang025@gmail.com>
2025-11-06 19:18:45 -0800

    made changes to readme

--- 02_funct_Plots.R
+++ 02_funct_Plots.R
@@ -1,8 +1,53 @@
 # for all functions for plotting 
 
-plot_bar <- function(data, x, y, fill) {
-  ggplot(data, aes(x = x, y = y, fill = fill)) +
-    geom_bar() +
-    theme_minimal() +
-    labs(title = "Bar Plot", x = x, y = y)
+
+# Bar graph
+plot_bar <- function(data, x, fill) {
+  ggplot(data, aes(x = x, fill = fill)) +
+    geom_bar()
 }
+
+
+# scatter/dot graph
+plot_scat <- function(data, x, y, focus) {
+  ggplot(data, aes(x = x, y = y)) + geom_point(aes(col = focus))
+}
+
+
+# box plot
+plot_box <- function(data, x, y) {
+  ggplot(data, aes(x = x, y = y)) + geom_boxplot()
+}
+
+
+# density plot
+plot_density <- function(data, focus) {
+  ggplot(data, aes(x = x)) + geom_density(aes(col = focus))
+}
+
+
+#density plot and density plot fill
+plot_2d <- function(data, focus) {
+  ggplot(data, aes(x = x)) + geom_density_2d(aes(col = focus))
+}
+
+plot_2d_fill <- function(data, focus) {
+  ggplot(data, aes(x = x)) + geom_density_2d_filled(aes(col = focus))
+}
+
+# Density plot fill with facet plot
+
+plot_facet_fill_density <- function(data, x, y, focus) {
+  ggplot(data, aes(x = x, y = y)) + geom_density_2d_filled() + facet_grid(cols = col)
+}
+
+# violin plot
+
+plot_violiin <- function(data, x, y) {
+  ggplot(data, aes(x = x, y = factor(y))) + geom_violin(aes(col = factor(y), fill = factor(y)))
+}
+
+
+
+
+
--- README.md
+++ README.md
@@ -4,3 +4,5 @@ A project for the course STATS107. This will take data from the FBI, analyze it,
 ### The point of this project is to take the data about hate crimes, provided by the FBI, clean the data, and compile it into something that could be used and understood.
 
 This project uses functions, and different libraries to run the code, and make it easier on the eyes to view the code when the functions are packed away in different files, all nice and neat.
+
+To start out, run the files, **00_requirements.R**, **01_funct_DataCeaning.R**, and **02_funct_Plots.R**. That way, all of the functions and libraries needed for this project are downloaded and ready for use.
