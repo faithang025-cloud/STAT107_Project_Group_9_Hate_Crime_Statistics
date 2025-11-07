a4c02af8689c651a73d10b91b8b7a04c181045bf
faithang025-cloud <faithang025@gmail.com>
2025-11-06 18:17:11 -0800

    added ggplot2, added potential plot, and made new file called 12_Plots.R

--- 02_funct_Plots.R
+++ 02_funct_Plots.R
@@ -1,1 +1,8 @@
 # for all functions for plotting 
+
+plot_bar <- function(data, x, y, fill) {
+  ggplot(data, aes(x = x, y = y, fill = fill)) +
+    geom_bar() +
+    theme_minimal() +
+    labs(title = "Bar Plot", x = x, y = y)
+}
---
+++ 12_Plots.R
@@ -0,0 +1,1 @@
+# for all ploting code and testing
